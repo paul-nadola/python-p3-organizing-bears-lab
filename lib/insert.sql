f819cc8070acfc6013244af55107fb52135723eb
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-05-21 14:32:30 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+bears ( name, age, sex, color, temperament, alive )
+VALUES
+('Mr. Chocolate', 5, 'M', 'brown', 'docile', True),
+('Rowdy', 5, 'M', 'black', 'fierce', True),
+('Tabitha', 6, 'F', 'grey', 'docile', False),
+('Sergeant Brown', 4, 'M', 'brown', 'fierce', True),
+('Melissa', 1, 'F', 'brown', 'docile', True),
+('Grinch', 7, 'M', 'white', 'polite', False),
+('Wendy', 2, 'F', 'white', 'fierce', False),
+( NULL, 8, 'F', 'black', 'docile', True);
+
